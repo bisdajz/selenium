048c4bfb0a390ae3a65dcda2a89460c4e097e95d
Biswajit <biswajitdajz5678@gmail.com>
2024-02-27 15:36:51 +0530

    add updated url

--- test/src/test/java/com/learning/test/learning1.java
+++ test/src/test/java/com/learning/test/learning1.java
@@ -1,6 +1,8 @@
 package com.learning.test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class learning1 {
@@ -10,8 +12,12 @@ public class learning1 {
 		 
 	        // Maximize the browser
 	        driver.manage().window().maximize();
-	        driver.get("https://www.geeksforgeeks.org/");
-
-	}
+	        driver.get("https://document-generator.hestawork.com/");
+	    
+	        WebElement button = driver.findElement(By.xpath("//a[@class=\"flex items-center gap-2 rounded-md border border-primary bg-transparent px-6 py-2 text-base font-bold text-primary \" and @href=\"/auth/signup\"]\r\n"	+ ""));
+	        button.click();
+	        
+	        
+	        	}
 
 }
